2b4ac5c3fd4ccea6d80b6cb85d210fbaf2ce0c1e
russ5 <15rjl8@queensu.ca>
2020-02-01 10:50:17 -0500

    fixed y

--- state.cpp
+++ state.cpp
@@ -126,7 +126,7 @@ void State::fireMissile( int siloIndex, float x, float y )
     // CHANGE THIS
 
     missilesOut.add( Missile( silos[siloIndex].position(),  // source
-			      speed * vec3(x-silos[siloIndex].position().x,silos[siloIndex].position().y-y,0),  // velocity
+			      speed * vec3(x-silos[siloIndex].position().x,(1-y)-silos[siloIndex].position().y,0),  // velocity
 			      y,  // destination y
 			      vec3( 0,1,1 ) ) );  // colour
   }
