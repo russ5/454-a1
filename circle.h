28bbd485148781a38a7dbe934efcdcb1ac7d077c
russ5 <32937206+russ5@users.noreply.github.com>
2020-02-01 14:12:43 -0500

    attempting colour fade v3

--- circle.h
+++ circle.h
@@ -29,6 +29,8 @@ class Circle {
   void draw( GPUProgram *gpuProgram ) {
 
     const int NUM_SEGMENTS = 90; // number of pieces of circle perimeter
+    const float dimmer = 0.5;
+    const float dimmest = 0.25;
 
     vec3 *verts = new vec3[NUM_SEGMENTS+1];
 
@@ -48,7 +50,7 @@ class Circle {
       verts[i+1] = vec3( pos.x + (2*r/3) * sin(theta), pos.y + (2*r/3) * cos(theta), 0 );
     }
 
-    gpuProgram->drawVertices( verts, NUM_SEGMENTS, GL_TRIANGLE_FAN, colour * 0.5 );
+    gpuProgram->drawVertices( verts, NUM_SEGMENTS, GL_TRIANGLE_FAN, dimmer * colour );
 
     verts[0] = pos;
 
@@ -57,7 +59,7 @@ class Circle {
       verts[i+1] = vec3( pos.x + r * sin(theta), pos.y + r * cos(theta), 0 );
     }
 
-    gpuProgram->drawVertices( verts, NUM_SEGMENTS, GL_TRIANGLE_FAN, colour * 0.25 );
+    gpuProgram->drawVertices( verts, NUM_SEGMENTS, GL_TRIANGLE_FAN, dimmest * colour );
 
     delete [] verts;
   }
