dd6ee053dbfc20314b6a5bf6fa68ee147bcd9734
russ5 <32937206+russ5@users.noreply.github.com>
2020-02-01 15:04:25 -0500

    attempting missile fade v2

--- missile.h
+++ missile.h
@@ -38,14 +38,14 @@ class Missile {
 
     gpuProgram->drawVertices( &verts[0], 2, GL_LINES, dimmest * colour );
 
-    vec3 verts[2] = {
+    verts[2] = {
       vec3( (pos1.x-pos0.x)*0.3, (pos1.y-pos0.y)*0.3, 0 ),
       vec3( pos1.x, pos1.y, 0 )
     };
 
     gpuProgram->drawVertices( &verts[0], 2, GL_LINES, dimmer * colour );
 
-    vec3 verts[2] = {
+    verts[2] = {
       vec3( (pos1.x-pos0.x)*0.1, (pos1.y-pos0.y)*0.1, 0 ),
       vec3( pos1.x, pos1.y, 0 )
     };
